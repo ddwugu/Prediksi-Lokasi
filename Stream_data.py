bef74c14ca57d664a2fa070c944f41b4457791a9
ddwugu <136143092+ddwugu@users.noreply.github.com>
2023-12-29 19:58:44 +0700

    Stream_data.py

--- Stream_data.py
+++ Stream_data.py
@@ -1,29 +1,32 @@
 import pickle
 import streamlit as st
 
-# Set background image
+try:
+    # Load the model
+    LokasiKM = pickle.load(open('Pred_lokasi.sav', 'rb'))
+except Exception as e:
+    st.error(f"Error loading the model: {e}")
 
-
-LokasiKM = pickle.load(open('Pred_lokasi.sav', 'rb'))
-
-# Judul Web
+# Web Title
 st.title('Pertamina Field Jambi')
 
+# User Inputs
 Titik_1_PSI = st.text_input('Input Pressure di titik 1 (PSI)')
 Titik_2_PSI = st.text_input('Input Pressure di titik 2 (PSI)')
 
-# Code prediction
+# Prediction Code
 suspect_loct = ''
 
-# Tombol prediksi
+# Prediction Button
 if st.button('Prediksi Lokasi'):
-    prediksi_lokasi = LokasiKM.predict([[float(Titik_1_PSI), float(Titik_2_PSI)]])
-
-    if prediksi_lokasi[0] == 0:
-        suspect_loct = 'Pipa Aman'
-    elif prediksi_lokasi[0] == 26.8:
-        suspect_loct = 'Tidak Terdapat Fluida yang Mengalir'
-    else:
-        suspect_loct = f'Terjadi di titik {prediksi_lokasi[0]} KM'
-
-    st.success(suspect_loct)
+    try:
+        prediksi_lokasi = LokasiKM.predict([[float(Titik_1_PSI), float(Titik_2_PSI)]])
+        if prediksi_lokasi[0] == 0:
+            suspect_loct = 'Pipa Aman'
+        elif prediksi_lokasi[0] == 26.8:
+            suspect_loct = 'Tidak Terdapat Fluida yang Mengalir'
+        else:
+            suspect_loct = f'Terjadi di titik {prediksi_lokasi[0]} KM'
+        st.success(suspect_loct)
+    except Exception as e:
+        st.error(f"Error predicting location: {e}")
